64df7b83e9e872f8ab762544ad512fc3eff03c90
SrFreiRe <juan.freire@rai.usc.es>
2025-07-28 21:08:17 +0200

    health check

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/version", (req, res) => {
   res.send("1.0.0");
 });
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
